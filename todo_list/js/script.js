f81361330ca8f4d28ac3cb9204a5e541678d4ecf
cnguyen1812 <102569963+cnguyen1812@users.noreply.github.com>
2022-03-29 12:45:40 +0800

    Added interactivity

--- todo_list/js/script.js
+++ todo_list/js/script.js
@@ -1,1 +1,14 @@
-// Empty script file to start with
+// Step 1: Find element
+let addTaskButton = document.getElementById("add-task")
+let newTaskInput = document.getElementById("task-input")
+
+
+// Step 2: Write a function to implemenet behavioiur
+function onAddTaskClicked(event){
+    let taskName = newTaskInput.value;
+    newTaskInput.value = "";
+    console.log(taskName)
+}
+
+// Step 3: Link element, function and event
+addTaskButton.addEventListener('click', onAddTaskClicked)
