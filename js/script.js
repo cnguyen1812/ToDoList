7b754a20670b986019e19d53f3a66d179a5452de
cnguyen1812 <102569963+cnguyen1812@users.noreply.github.com>
2022-03-29 13:33:45 +0800

    Delete folder

--- js/script.js
+++ js/script.js
@@ -3,12 +3,16 @@ let addTaskButton = document.getElementById("add-task")
 let newTaskInput = document.getElementById("task-input")
 let toDoListContainer = document.getElementById("todo-list")
 
+let templateElement = document.getElementById("ListItemTemplate")
+let template = templateElement.innerHTML;
+
 // Step 2: Write a function to implemenet behavioiur
 function onAddTaskClicked(event){
     let taskName = newTaskInput.value;
     newTaskInput.value = "";
-    console.log(taskName)
-    toDoListContainer.insertAdjacentHTML("afterbegin", taskName);
+
+    let taskHTML = template.replace("<!-- Task_Name-->",taskName);
+    toDoListContainer.insertAdjacentHTML("afterbegin", taskHTML);
 }
 
 // Step 3: Link element, function and event
