d38fd1cdc47007fc821779e9b509e7c715d16a46
cnguyen1812 <102569963+cnguyen1812@users.noreply.github.com>
2022-04-05 12:40:42 +0800

    comment

--- js/script.js
+++ js/script.js
@@ -68,7 +68,7 @@
         }
     }
 
-     
+     // Fix This !!
     function showCompletedTasks(){
         let tasks = document.getElementsByClassName("task")
         for (let i=0; i < tasks.length; i++){
