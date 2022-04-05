f0e58820d9739ec3cc7ea29b83d2d2549cfa950b
cnguyen1812 <102569963+cnguyen1812@users.noreply.github.com>
2022-04-05 12:20:56 +0800

    button

--- js/script.js
+++ js/script.js
@@ -55,7 +55,11 @@
     function showCompletedTasks(){
         let tasks = document.getElementsByClassName("task")
         for (let i=0; i < tasks.length; i++){
+            if (tasks[i].classList.contains("completed")){
                 tasks[i].style.display = "block"
+            } else {
+                tasks[i].style.display = "none"
+            }
         }
     }
     
